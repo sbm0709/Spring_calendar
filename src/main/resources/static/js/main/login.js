e687bcdfd34f9933f8335a0f557da6a7c0bec141
Administrator <simple852@naver.com>
2023-12-06 16:09:47 +0900

    완...?

---
+++ src/main/resources/static/js/main/login.js
@@ -0,0 +1,55 @@
+const signUpBox = document.querySelector(".sign-up-box");
+
+signUpBox.addEventListener("click", handleClick, false);
+function handleClick(event) {
+    if (signUpBox.classList.contains("active")) return;
+    let str = `
+            <span onclick="event.stopPropagation(); removedActive();">X</span>
+            <input type="text" name="id" placeholder="ID"/>
+            <input type="password" name="pw" placeholder="PASSWORD"/>
+            <input type="file" name="fileData"/>
+<!--            <input type="submit" value="들어가 제발" />-->
+            <button onclick="handleSignUp()">제발 들어가줘</button>
+    `;
+    this.classList.toggle("active");
+    this.innerHTML = "";
+    setTimeout(() => (this.innerHTML = str), 500);
+}
+
+function handleSignUp() {
+    let signUpBoxActive = document.querySelector(".active");
+    let inputs = signUpBoxActive.querySelectorAll("input");
+    let form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', "/user/register");
+    form.setAttribute('enctype', "multipart/form-data");
+
+    let idField = inputs[0];
+    let pwField = inputs[1];
+    let fileField = inputs[2];
+
+    // if (
+    //     idField.value === "" ||
+    //     pwField.value === "" ||
+    //     fileField.value === ""
+    // ) {
+    //     return;
+    // }
+
+    form.appendChild(idField);
+    form.appendChild(pwField);
+    form.appendChild(fileField);
+    document.body.appendChild(form);
+    form.encoding = "multipart/form-data";
+    console.log(form.encoding)
+    form.submit();
+    removedActive();
+
+}
+
+
+
+function removedActive() {
+    signUpBox.classList.remove("active");
+    signUpBox.innerHTML = `<i class="fa-solid fa-pen"></i>`;
+}
